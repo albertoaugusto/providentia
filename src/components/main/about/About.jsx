c3febf7a8cb653d367c613025b77e279c4d2bced
Alberto Souza <albertosouza16.contato@gmail.com>
2024-01-08 18:29:47 -0300

    about v2

--- src/components/main/about/About.jsx
+++ src/components/main/about/About.jsx
@@ -9,22 +9,31 @@ const About = () => {
         <h3 className="about-title">
             Quem somos nós:            
         </h3>
-        <div className="about-container grid">
-            <div className="about-info">
-                <p className='about-description'>
-                    Nossa esquipe altamente qualificada possui <span className='orange'> 
-                    ampla experiência</span> no ramo e está pronta
-                    para auxiliá-lo(a) em todos os aspectos do processo 
-                    de registro, desde a pesquisa até o acompanhamento junto 
-                    aos órgãos competentes.     
-                </p> 
+        <div className="about-container">
+            <div className="row">
+                <div className="card-about">
+                    <div className="item-in">
+                        <h4 className='card-title-about'>Nossa equipe</h4>
+                        <div className="seperator"></div>
+                        <p className='card-text'>
+                            Nossa esquipe altamente qualificada possui <b>ampla experiência</b> no ramo e 
+                            está pronta para auxiliá-lo(a) em todos os aspectos do processo de registro,
+                            desde a pesquisa até o acompanhamento junto aos órgãos competentes.
+                        </p>
+                    </div>
+                </div>
+                <div className="card-about">
+                    <div className="item-in">
+                        <h4 className='card-title-about'>Nosso diferencial</h4>
+                        <div className="seperator"></div>
+                        <p className='card-text'>
+                            Garantimos atendimento personalizado, agilidade nos processos e 
+                            compromisso com a excelência. 
+                            Proteja sua marca e inovações com a Providentia Consultoria.
+                        </p>
+                    </div>
+                </div>
             </div>
-            <div className="about-info">
-                 <p className="about-description">
-                    Garantimos atendimento <span className='orange'>personalizado</span>, <span className='orange'> agilidade nos processos </span> e <span className='orange'>compromisso</span> com 
-                    a excelência. Proteja sua marca e inovações com a <b className='providentia'>Providentia Consultoria.</b>
-                </p>      
-            </div>   
         </div>
     </section>
   )
